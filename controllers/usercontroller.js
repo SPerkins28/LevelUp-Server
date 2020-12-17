4ceae4240e72046fbc267295dbe4dc0b6290b6c2
Sean Perkins <seanperkins@Seans-MacBook-Pro.local>
2020-12-16 23:18:48 -0500

    finished user model and controller -- added ability to update username and update password with rehash

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const {User} = require('../models');
+const validateSession = require('../middleware/validateSession')
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -51,7 +52,7 @@ router.post('/login', async (req, res) => {
                 {expiresIn: 60 * 60 * 24}
             );
             res.status(201).json({
-                message: "Player One Ready!",
+                message: "Ready Player One!",
                 user: loginUser,
                 sessionToken: token,
             });
@@ -67,3 +68,40 @@ router.post('/login', async (req, res) => {
     }
 });
 
+/* UPDATE USERNAME */
+router.put('/username/:id', validateSession, async (req, res) => {
+    const query = req.params.id
+
+    User.update(req.body, {where: {id: query}})
+        .then((usernameUpdated) => {
+            User.findOne({where: {id: query}})
+                .then((locatedUpdatedUsername) => {
+                    res.status(200).json({
+                        username: locatedUpdatedUsername,
+                        message: "Player 1 username updated successfully!",
+                        usernameChanged: usernameUpdated,
+                    });
+                });
+        })
+        .catch((err) => res.json(err));
+})
+
+/* UPDATE PASSWORD */
+router.put('/password/:id', validateSession, async (req, res) => {
+    const query = req.params.id
+
+    User.update({password: bcrypt.hashSync(req.body.password, 13)}, {where: {id: query}})
+        .then((passwordUpdated) => {
+            User.findOne({where: {id: query}})
+                .then((locatedUpdatedPassword) => {
+                    res.status(200).json({
+                        password: locatedUpdatedPassword,
+                        message: "Player 1 password updated successfully!",
+                        passwordChanged: passwordUpdated,
+                    });
+                });
+        })
+        .catch((err) => res.json(err));
+})
+
+module.exports = router;
