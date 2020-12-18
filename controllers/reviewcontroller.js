4f418921dc3f929466473adc398788e92e50a479
Sean Perkins <seanperkins@Seans-MacBook-Pro.local>
2020-12-17 21:32:51 -0500

    added endpoints for GET reviews by userId, PUT(update) reviews by review id, and DELETE reviews by id

--- controllers/reviewcontroller.js
+++ controllers/reviewcontroller.js
@@ -27,7 +27,7 @@ router.post('/create', validateSession, async (req, res) => {
 }),
 
 /* GET ALL REVIEWS FOR GAME */
-router.get('/:gameId', validateSession, async (req, res) => {
+router.get('/games/:gameId', validateSession, async (req, res) => {
     const gameId = req.params.gameId;
     console.log(gameId)
     try {
@@ -41,6 +41,70 @@ router.get('/:gameId', validateSession, async (req, res) => {
             message: "You opened the chest and found nothing but dust...",
             error: error
         });
+    }
+}),
+
+/* GET ALL REVEIWS BY USER ID */
+router.get('/user/:userId', validateSession, async (req, res) => {
+    const userId = req.params.userId;
+    console.log(userId);
+    try {
+        let userReviews = await Review.findAll({where: {userId: userId}, include: 'user'})
+        res.status(200).json({
+            userReviews: userReviews,
+            message: "You found 5g and all of these reviews!"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "You found nothing, move along...",
+            error: error
+        });
+    }
+}),
+
+/* UPDATE REVIEWS BY REVIEW ID */
+router.put('/:id', validateSession, async (req, res) => {
+    try {
+        const query = req.params.id;
+        await Review.update(req.body, {where: {id: query}})
+        .then((updatedReview) => {
+            Review.findOne({ where: {id: query} })
+            .then((locatedUpdatedReview) => {
+                res.status(200).json({
+                    editedReview: locatedUpdatedReview,
+                    message: "Quest Completed: Review Updated!",
+                    updatedReview: updatedReview,
+                })
+            })
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Quest Failed: Review not updated!",
+            error: error
+        });
+    }
+}),
+
+/* DELETE REVIEWS BY REVEIW ID */
+router.delete('/:id', validateSession, async (req, res) => {
+    try {
+        const query = req.params.id;
+        await Review.destroy({where: {id: query}})
+        .then((deletedReview) => {
+            Review.findOne({where: {id: query} })
+            .then((locatedDeletedReview) => {
+                res.status(200).json({
+                    deletedReview: deletedReview,
+                    message: "Review destroyed successfully! You received 5 useless parts...",
+                    locatedDelete: locatedDeletedReview
+                })
+            })
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: error,
+            message: "Review not destroyed. Try better next time!"
+        })        
     }
 }),
 
